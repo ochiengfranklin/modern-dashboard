99188132d60dc2d01506b3e922fb7b49ceca85f1
Franklin Ochieng <ochiengfranklin264@gmail.com>
2026-04-06 17:15:25 +0300

    implement upload cloudinary widget

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,13 @@ import { useNotificationProvider } from "./components/refine-ui/notification/use
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 
 import Dashboard from "@/pages/dashboard.tsx";
-import {BookOpen, Home} from "lucide-react";
+import {BookOpen, GraduationCap, Home} from "lucide-react";
 import {Layout} from "@/components/refine-ui/layout/layout.tsx";
 import SubjectsList from "@/pages/subjects/list.tsx";
 import SubjectsCreate from "@/pages/subjects/create.tsx";
+
+import ClassesList from "@/pages/classes/list.tsx";
+import ClassesCreate from "@/pages/classes/create.tsx";
 import {dataProvider} from "@/providers/data.ts";
 
 
@@ -49,6 +52,12 @@ function App() {
                       list: "/subjects",
                       create: "/subjects/create",
                       meta: { label: "Subjects", icon: <BookOpen />}
+                  },
+                  {
+                      name: "classes",
+                      list: "/classes",
+                      create: "/classes/create",
+                      meta: { label: "classes", icon: <GraduationCap />}
                   }
               ]}
             >
@@ -63,6 +72,11 @@ function App() {
                     <Route path="subjects">
                         <Route index element={<SubjectsList />} />
                         <Route path="create" element={<SubjectsCreate />} />
+                    </Route>
+
+                    <Route path="classes">
+                        <Route index element={<ClassesList />} />
+                        <Route path="create" element={<ClassesCreate />} />
                     </Route>
                 </Route>
 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,23 +1,23 @@
-// import { GraduationCap, School } from "lucide-react";
+import { GraduationCap, School } from "lucide-react";
 
-// export const USER_ROLES = {
-//     STUDENT: "student",
-//     TEACHER: "teacher",
-//     ADMIN: "admin",
-// };
+export const USER_ROLES = {
+    STUDENT: "student",
+    TEACHER: "teacher",
+    ADMIN: "admin",
+};
 
-// export const ROLE_OPTIONS = [
-//     {
-//         value: USER_ROLES.STUDENT,
-//         label: "Student",
-//         icon: GraduationCap,
-//     },
-//     {
-//         value: USER_ROLES.TEACHER,
-//         label: "Teacher",
-//         icon: School,
-//     },
-// ];
+export const ROLE_OPTIONS = [
+    {
+        value: USER_ROLES.STUDENT,
+        label: "Student",
+        icon: GraduationCap,
+    },
+    {
+        value: USER_ROLES.TEACHER,
+        label: "Teacher",
+        icon: School,
+    },
+];
 
 export const DEPARTMENTS = [
     "Computer Science",
@@ -63,17 +63,17 @@ const getEnvVar = (key: string): string => {
     return value;
 };
 
-// export const CLOUDINARY_UPLOAD_URL = getEnvVar("VITE_CLOUDINARY_UPLOAD_URL");
-// export const CLOUDINARY_CLOUD_NAME = getEnvVar("VITE_CLOUDINARY_CLOUD_NAME");
+ export const CLOUDINARY_UPLOAD_URL = getEnvVar("VITE_CLOUDINARY_UPLOAD_URL");
+ export const CLOUDINARY_CLOUD_NAME = getEnvVar("VITE_CLOUDINARY_CLOUD_NAME");
 export const BACKEND_BASE_URL = getEnvVar("VITE_BACKEND_BASE_URL");
 
-// export const BASE_URL =  import.meta.env.VITE_API_URL;
-// export const ACCESS_TOKEN_KEY = import.meta.env.VITE_ACCESS_TOKEN_KEY
-// export const REFRESH_TOKEN_KEY = import.meta.env.VITE_REFRESH_TOKEN_KEY
+export const BASE_URL =  import.meta.env.VITE_API_URL;
+export const ACCESS_TOKEN_KEY = import.meta.env.VITE_ACCESS_TOKEN_KEY
+export const REFRESH_TOKEN_KEY = import.meta.env.VITE_REFRESH_TOKEN_KEY
 
-// export const REFRESH_TOKEN_URL = `${BASE_URL}/refresh-token`;
-//
-// export const CLOUDINARY_UPLOAD_PRESET = getEnvVar("VITE_CLOUDINARY_UPLOAD_PRESET");
+export const REFRESH_TOKEN_URL = `${BASE_URL}/refresh-token`;
+
+export const CLOUDINARY_UPLOAD_PRESET = getEnvVar("VITE_CLOUDINARY_UPLOAD_PRESET");
 
 export const teachers = [
     {
